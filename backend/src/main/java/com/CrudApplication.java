3dc2076a95a4365f7a5dcffc2667619cbd6e8211
Rizwan Yousaf <rizwan.ravianpk@gmail.com>
2022-09-11 22:23:37 +0200

    fixed issue

--- backend/src/main/java/com/CrudApplication.java
+++ backend/src/main/java/com/CrudApplication.java
@@ -2,11 +2,9 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.CastMember"})
-public class SpringbootBackendApplication {
+public class CrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrudApplication.class, args);
